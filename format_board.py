4ea16bc2ad0402e33102384f9e5327073a72068b
Stine Føns Nielsen <stinesmail@hotmail.com>
2025-04-23 17:58:47 +0200

    Update format_board.py
    Better visual formatting

--- format_board.py
+++ format_board.py
@@ -1,9 +1,16 @@
 def format_board(board):
     joined_rows = []
-    divider = "\n-"
+    divider = "\n   -"
+    cols = "   "
+    
+    for i in range(len(board)):
+        cols += str(i+1) + " "
+        board[i].insert(0,str(i+1) + " ")
+    joined_rows.append(cols)  
+    
     for row in board:
         joined_rows.append("|".join(row))
-    
+   
     for _ in range(len(board)-1):
         divider += "+-"
     divider += "\n"
