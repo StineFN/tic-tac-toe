73864273161aa86a8c6db8df26b2082e3e7d8d71
StineFN <stinesmail@hotmail.com>
2025-04-26 18:47:14 +0200

    Added a sandbox to test the output and added tests for correct formatting of the board

---
+++ sandbox.py
@@ -0,0 +1,10 @@
+from format_board import format_board
+
+
+testboard = [
+        ['X', ' ', 'O'],
+        ['X', 'X', 'X'],
+        ['O', ' ', 'O']
+]
+
+print(repr(format_board(testboard)))
---
+++ test_format_board.py
@@ -0,0 +1,22 @@
+from format_board import format_board
+
+def test_format_board():
+    board = [
+        ['X', ' ', 'O'],
+        ['X', 'X', 'X'],
+        ['O', ' ', 'O']
+    ]
+
+    board1 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'X', 'O'],
+        ['X', 'X', 'O', 'O'],
+        ['O', ' ', 'O', ' ']
+    ]
+
+    result = format_board(board)
+    assert result == "   1 2 3\n   -+-+-\n1 |X| |O\n   -+-+-\n2 |X|X|X\n   -+-+-\n3 |O| |O"
+
+    result1 = format_board(board1)
+    assert result1 == "   1 2 3 4\n   -+-+-+-\n1 |X| |O|X\n   -+-+-+-\n2 |X|X|X|O\n   -+-+-+-\n3 |X|X|O|O\n   -+-+-+-\n4 |O| |O| "
+    
