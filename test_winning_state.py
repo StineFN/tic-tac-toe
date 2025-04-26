bfc213472b1d70193079df16545cefa3586fc223
StineFN <stinesmail@hotmail.com>
2025-04-26 18:17:41 +0200

    Added tests for all four functions to check a winning board

--- test_winning_state.py
+++ test_winning_state.py
@@ -1,4 +1,7 @@
 from winning_state import row_winner 
+from winning_state import column_winner
+from winning_state import diagonal_winner
+from winning_state import winner
 
 def test_row_winner():
     board = [
@@ -10,7 +13,15 @@ def test_row_winner():
     board1 = [
         ['X', ' ', 'O', 'X'],
         ['X', 'X', 'X', 'O'],
+        ['X', 'X', 'O', 'O'],
         ['O', ' ', 'O', ' ']
+    ]
+
+    board2 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'O', 'X', 'O'],
+        ['X', 'X', 'O', 'O'],
+        [' ', ' ', ' ', ' ']
     ]
 
     result = row_winner(board)
@@ -18,3 +29,99 @@ def test_row_winner():
 
     result1 = row_winner(board1)
     assert result1 == False
+
+    result2 = row_winner(board2)
+    assert result2 == False
+
+def test_column_winner():
+
+    board = [
+        ['X', ' ', 'O'],
+        ['X', ' ', 'X'],
+        ['O', ' ', 'O']
+    ]
+
+    board1 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'O', 'O'],
+        ['X', 'X', 'O', 'O'],
+        ['O', ' ', 'O', ' ']
+    ]
+
+    board2 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'X', 'X'],
+        ['X', 'X', 'O', 'O'],
+        [' ', ' ', ' ', ' ']
+    ]
+
+    result = column_winner(board)
+    assert result == False
+
+    result1 = column_winner(board1)
+    assert result1 == True
+
+    result2 = column_winner(board2)
+    assert result2 == False
+
+def test_diagonal_winner():
+
+    board = [
+        ['X', ' ', 'O'],
+        ['X', 'O', 'X'],
+        ['O', ' ', 'O']
+    ]
+
+    board1 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'O', 'O'],
+        ['X', 'X', 'X', 'O'],
+        ['O', ' ', 'O', 'X']
+    ]
+
+    board2 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'X', 'X'],
+        ['X', 'X', 'O', 'O'],
+        [' ', ' ', ' ', ' ']
+    ]
+
+    result = diagonal_winner(board)
+    assert result == True
+
+    result1 = diagonal_winner(board1)
+    assert result1 == True
+
+    result2 = diagonal_winner(board2)
+    assert result2 == False
+
+def test_winner():
+
+    board = [
+        ['X', ' ', 'O'],
+        ['X', 'O', 'X'],
+        ['O', ' ', 'O']
+    ]
+
+    board1 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'X', 'O', 'O'],
+        ['X', 'X', 'O', 'O'],
+        ['O', ' ', 'O', 'X']
+    ]
+
+    board2 = [
+        ['X', ' ', 'O', 'X'],
+        ['X', 'O', 'X', 'X'],
+        ['X', 'X', 'O', 'O'],
+        [' ', ' ', ' ', ' ']
+    ]
+
+    result = winner(board)
+    assert result == True
+
+    result1 = winner(board1)
+    assert result1 == True
+
+    result2 = winner(board2)
+    assert result2 == False
