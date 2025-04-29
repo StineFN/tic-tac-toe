db9c8305ffb71c889181321b54fe37cdd6ba327e
StineFN <stinesmail@hotmail.com>
2025-04-29 15:44:53 +0200

    Fixed format board so it looks pretty!

--- format_board.py
+++ format_board.py
@@ -1,35 +1,23 @@
-"""
 def format_board(board):
-    joined_rows = []
-    divider = "\n   -"
-    cols = "   "
     size = len(board)
-    
-    for i in range(size):
-        cols += str(i+1) + " "
-        #board[i].insert(0,str(i+1) + " ")
-    joined_rows.append(cols.rstrip())
-    
-    for row in board:
-       joined_rows.append("|".join(row))
-   
+    first_row = '  '
+    divider = "\n  -"
+
+    # Create the divier line in the right size
     for _ in range(size-1):
         divider += "+-"
     divider += "\n"
-    
-    string = divider.join(joined_rows)
-    return string
 
-"""
-def format_board(board):
-    first_row = ' '
-    for i in range(len(board)):
-        first_row += str(i + 1)
+    # Create the first row
+    for i in range(size):
+        first_row += str(i + 1) + ' '
+
     joined_rows = [first_row]
-    for i in range(len(board)):
-        joined_row = str(i + 1) + ''.join(board[i])
+
+    for i in range(size):
+        joined_row = str(i + 1) + ' ' + '|'.join(board[i])
         joined_rows.append(joined_row)
-    return "\n".join(joined_rows)
+    return divider.join(joined_rows)
 
 
 """def format_board(board):
